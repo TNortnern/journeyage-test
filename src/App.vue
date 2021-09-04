c6281fc5f7c1d5811ea3b3b41b8fa7a0ba4fc28a
tnorthern <traynorthern@yahoo.com>
2021-09-04 12:57:42 -0500

    Remove boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-7xl mx-auto pt-8">
-    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+    <div class="relative text-sm bg-gray-200 py-2 px-4 shadow rounded-xl">
       <div>Hey How are you today?</div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,32 +9,8 @@ module.exports = {
       fontFamily: {
         sans: ["Helvetica", "Arial", "sans-serif"],
       },
-      backgroundColor: {
-        "primary-light": "#004EEF",
-        primary: "#03256C",
-        secondary: "#FFBC4F",
-        "secondary-dark": "#FCA311",
-        gray: "#f2f2f2",
-      },
       padding: {
         "app-container": "3.5rem"
-      },
-      textColor: {
-        primary: "#212B36",
-        secondary: "#707881",
-        active: "#03256C",
-        cta: "#FCA311",
-        warning: "#412801",
-        white: "#ffffff",
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-        "3xl": "1900px",
-        "4xl": "2560px",
       },
       colors: {
         "app-blue": "#03256C",
